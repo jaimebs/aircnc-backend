45bc392f06ad5e8640eab14a80ee0a09e744c378
Jaime Barbosa <jaime.barbosa@solsis.br>
2019-10-08 15:53:10 -0300

    chore: Added bodyParse to the project.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const bodyParse = require('body-parser');
 const cors = require('cors');
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParse.urlencoded({ extended: true }));
+app.use(bodyParse.json());
 app.use(require('./router'));
 
 app.listen(3333);
