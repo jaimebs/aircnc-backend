2215de213ece725098092d65f26777cde27594c7
Jaime Barbosa <jaime.barbosa@solsis.br>
2019-10-09 14:32:02 -0300

    refactor: extracted connection database for file database.js

--- src/server.js
+++ src/server.js
@@ -1,17 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParse = require('body-parser');
-const mongoose = require('mongoose');
 const cors = require('cors');
+const connection = require('./database');
 
 const app = express();
 
-mongoose.connect(process.env.CONNECTION_STRING, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
+connection();
 
 app.use(cors());
 app.use(bodyParse.urlencoded({ extended: true }));
