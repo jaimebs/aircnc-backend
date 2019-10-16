102276688cc06243bc0de636c36ebfa6f72b475f
Jaime Barbosa <jaime.barbosa@solsis.br>
2019-10-16 09:07:23 -0300

    refactor: created populate for user and spot.

--- src/controllers/booking.js
+++ src/controllers/booking.js
@@ -8,6 +8,12 @@ module.exports = {
       const { date } = req.body;
 
       const booking = await Booking.create({ user: user_id, spot: spot_id, date });
+
+      await booking
+        .populate({ path: 'user', select: '-password -createdAt -updatedAt -__v' })
+        .populate({ path: 'spot', select: '-createdAt -updatedAt -__v' })
+        .execPopulate();
+
       return res.status(201).json(booking);
     } catch (error) {
       return res.status(400).json(error);
